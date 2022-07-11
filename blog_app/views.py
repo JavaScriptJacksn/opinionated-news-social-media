4ed9e310c5f337edf8b9b9381fbffc827f940c53
JavaScriptJacksn <jbjackson247@gmail.com>
2022-07-11 16:53:18 +0000

    Intial template setup

--- blog_app/views.py
+++ blog_app/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
-
+import os
+if os.path.isfile("env.py"):
+    import env
 # Create your views here.
+
+def index(request):
+    return render(request, 'index.html')
