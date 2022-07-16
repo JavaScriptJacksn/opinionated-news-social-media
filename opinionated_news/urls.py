2bfe66c8babd78ca2357baec8ccd1ee9a4cdc3c2
JavaScriptJacksn <jbjackson247@gmail.com>
2022-07-16 18:29:31 +0000

    Added User authentication and comment functionality

--- opinionated_news/urls.py
+++ opinionated_news/urls.py
@@ -20,4 +20,5 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog_app.urls'), name="blog_app_urls"),
     path('summernote/', include('django_summernote.urls')),
+    path('accounts/', include('allauth.urls')),
 ]
